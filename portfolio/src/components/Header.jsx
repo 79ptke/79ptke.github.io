3c0b3fe2ee92044fbf269e7f9b2fcbf6ae221301
Jung da hee <82435745+79ptke@users.noreply.github.com>
2024-04-21 21:38:26 +0900

    update files

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,99 @@
+import '../styles/common.css';
+import '../styles/header.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faTerminal} from "@fortawesome/free-solid-svg-icons";
+import IconSVG from '../components/IconSvg';
+
+function Header() {
+
+    return (
+        <div className="AppHeader">
+            <div className="ApperHeader_wrapper">
+                <div className="AppHeader_globalBar flex">
+                    <div className="AppHeader_globarBar_start flex">
+                        <button type="button"className="AppHeader_btn AppHeader_icon_menu">
+                                <FontAwesomeIcon icon={faBars} />
+                        </button>
+                        <a href="#!">
+                            {/* <img src={logo} alt="로고" className="logo"/> */}
+                            <IconSVG id="logo" width="32" height="32"/>
+                        </a>
+                        <p className="AppHeader_context_item_label">JungDaHee / <b>Portfolio</b></p>
+                    </div>
+                    <div className="AppHeader_globalBar_end flex">
+                        <div className="AppHeader_btn AppHeader_search_wrapper">
+                            <button type="button"className="AppHeader_icon_search AppHeader_btnIcon">
+                                <IconSVG id="icon-search"/>
+                            </button>
+                            <div className="AppHeader_search_text_wrapper flex">
+                                <p className="AppHeader_search_text">Type <span>/</span> to search</p>
+                                <div className="AppHeader_search_text_icon">
+                                    <FontAwesomeIcon icon={faTerminal} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="AppHeader_icon_wrapper flex">
+                            <div className="AppHeader_btn AppHeader_icon_btn_wrapper">
+                                <button type="button"className="AppHeader_icon_plus AppHeader_btnIcon">
+                                    <IconSVG id="icon-plus"/>
+                                </button>
+                                <button type="button"className="AppHeader_icon_arrow AppHeader_btnIcon">
+                                    <IconSVG id="icon-arrow"/>
+                                </button>
+                            </div>
+                            <button type="button"className="AppHeader_btn AppHeader_icon_issues AppHeader_btnIcon">
+                                <IconSVG id="icon-issues"/>
+                            </button>
+                            <button type="button"className="AppHeader_btn AppHeader_icon_pulls AppHeader_btnIcon">
+                                <IconSVG id="icon-pullrequests"/>
+                            </button>
+                        </div>
+                        <button type="button"className="AppHeader_btn AppHeader_icon_notification AppHeader_btnIcon">
+                            <IconSVG id="icon-notifications"/>
+                        </button>
+                        <div className="AppHeader_btn AppHeader_profile_img"></div>
+                    </div>
+                </div>
+                <div className="AppHeader_localBar">
+                    <ul className="UnderlineNav_body flex">
+                        <li className="AppHeader_btnIcon">
+                            <IconSVG id="icon-code"/>
+                            Code
+                        </li>
+                        <li className="AppHeader_btnIcon">
+                            <IconSVG id="icon-issues"/>
+                            Issues
+                        </li>
+                        <li className="AppHeader_btnIcon">
+                            <IconSVG id="icon-pullrequests"/>
+                            Pull requests
+                        </li>
+                        <li className="AppHeader_btnIcon">
+                            <IconSVG id="icon-actions"/>
+                            Actions
+                        </li>
+                        <li className="AppHeader_btnIcon">
+                            <IconSVG id="icon-projects"/>
+                            Projects
+                        </li>
+                        <li className="AppHeader_btnIcon">
+                            <IconSVG id="icon-security"/>
+                            Security
+                        </li>
+                        <li className="AppHeader_btnIcon">
+                            <IconSVG id="icon-insights"/>
+                            Insights
+                        </li>
+                        <li className="AppHeader_btnIcon">
+                            <IconSVG id="icon-settings"/>
+                            Settings
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
---
+++ portfolio/src/components/MyProject.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import MyProjectContents from './MyProjectContents';
+import companyData from '../data/projectCompany.json';
+import personalData from '../data/projectPersonal.json';
+import '../styles/common.css';
+import "../styles/project.css";
+
+function Line({ lineNumber, text }) {
+    return (
+        <div>
+            <div className='lineArea grid'>
+                <div className="lineNumber">{lineNumber}</div>
+                <div className=''>{text}</div>
+            </div>
+        </div>
+    );
+}
+
+function MyProject() {
+
+    const [toggle, setToggle] = useState(false);
+
+    const handleToggle = () => {
+        setToggle(!toggle);
+    };
+
+    const lines = [
+        <div className='contents_title_wrapper flex'><h1 className='contents_title'>{toggle ? "MY PROJECT ✨":"WORK EXPERIENCE 🏙"}</h1><button className={`toggle_button ${toggle ? 'on' : 'off'}`} onClick={handleToggle}> <span>{toggle ? "회사 프로젝트 보러가기" : "개인 프로젝트 보러가기"}</span> <span className='arrow'>&#10095;</span></button></div>,
+        "",
+        "",
+        toggle ?  <div>주로 리액트 위주의 개인 작업물입니다.</div> : <div><h2>집쇼코리아 (2021.08 ~ 2023.11)</h2></div>,
+        toggle ?  null : <div>웹 퍼블리셔</div>,
+        toggle ?  null : "",
+        toggle ?  null : "",
+        toggle ?  null : <div><h3>주요 업무 프로젝트</h3></div>,
+        toggle ?  null : "",
+        toggle ?  null : <div>- html, css, js, react, Bootstrap, api을 사용한 반응형 웹사이트 및 CMS 구축 웹 프로젝트에 참여. 일부 기획 및 구현, 하드 코딩된 코드를 React로 고도화</div>,
+        toggle ?  null : "",
+        toggle ?  null : <div>- 웹사이트, 키오스크, 미디어 월 등 반응형 및 모바일 최적화, 크로스 브라우징, Android와 iOS 플랫폼 등다중 플랫폼 지원</div>,
+        toggle ?  null : "",
+        toggle ?  null : <div>- matterport, krpano MXL, 3dvista를 활용하여 웹 페이지 구축</div>,
+        toggle ?  null : "",
+        toggle ?  null : "",
+        toggle ?  null : <div><h3>주요 성과</h3></div>,
+        toggle ?  null : "",
+        toggle ?  null : <div>- 키오스트에서 유저들이 불편함을 호소. 사용자의 불편함을 개선하기 위하여,디자이너와 협업하여 화면 기획. 인터페이스의 복잡성을 간소화하여 정보의 시각적 가시성을 높이고, 사용자가 필요로 하는 터치 횟수를 최소화하여 키오스크 사용 경험을 개선하여 사용자 편의 성을 높임.</div>,
+        toggle ?  null : "",
+        toggle ?  null : <div>- 3d 가상공간에서 무분별한 태그 사용으로 인한 사용감 저하. 이미지, 동영상, 텍스트, 언어 구분이 모두 분리되어 있는 문제 발견. 분리되어 있는 정보를 효과적으로 합쳐 하나의 팝업으로 제작. 하나의 유물에 기본 5개 이상의 태그가 필요하던 것을 태그 하나로 해결.</div>,
+        toggle ?  null : "",
+        toggle ?  null : <div>- 팀원간코드스타일일관성부족및공유가안되는문제. 지속적인회의를통한코드리뷰 프로세스 제안 및 코드 스타일 가이드 수립. 팀원 간 코드 이해도 향상, 코드 품질 및 일관성을 지속해서 개선.</div>,
+        toggle ?  null : "",
+        toggle ?  null : "",
+    ];
+
+    return (
+        <div className="myProject">
+            {lines.map((line, index) => (
+                line !== null &&  <Line key={index + 1} lineNumber={index + 1} text={line} />
+            ))}
+            <div className="MyProject_contents_wrapper">
+            {toggle ? (
+                    <MyProjectContents projects={personalData.projectPersonal} />
+                ) : (
+                    <MyProjectContents projects={companyData.projectCompany} />
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default MyProject;
