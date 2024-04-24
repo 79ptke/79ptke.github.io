198be1260c9cc0b1dfdcba928a771623aa64778c
Jung da hee <82435745+79ptke@users.noreply.github.com>
2024-04-25 00:41:31 +0900

    add project txt

--- src/components/MyProjectContents.jsx
+++ src/components/MyProjectContents.jsx
@@ -8,6 +8,13 @@ function MyProjectContents({ projects }) {
                     <div className="txtArea">
                         <h3 className="project_title">{project.title} <span>{project.date}</span> </h3>
                         <ul className="txtWrapper">
+                            {project.task &&<li className="project_description"><b>&bull; {project.task}</b></li>}
+                            {project.contribution &&<li className="project_description"><b>&bull; {project.contribution}</b></li>}
+                            {project.period &&<li className="project_description"><b>&bull; {project.period}</b></li>}
+                            {project.func &&<li className="project_description"><b>&bull; {project.func}</b></li>}
+                            {project.designtool &&<li className="project_description"><b>&bull; {project.designtool}</b></li>}
+                            {project.editor &&<li className="project_description"><b>&bull; {project.editor}</b></li>}
+                            {project.build &&<li className="project_description"><b>&bull; {project.build}</b></li>}
                             {project.description.map((desc, index) => (
                                 <li key={index} className="project_description">&bull; {desc}</li>
                             ))}
